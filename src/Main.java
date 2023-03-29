2d9a8f1b4f10f7195f7c4d549b861a2ae00e420b
MaryamRahimi2023 <maryam.qambari2009@gmail.com>
2023-03-29 12:36:06 -0400

    solved the issue

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         sunFeatures fero=new sunFeatures();
         fero.color="red";
-        fero.temperature=34234;
+        fero.temperature="34234";
         fero.heat="3243243243k";
 
     }
